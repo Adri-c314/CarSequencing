94a850c75c639d85f0e739274ba93a41e9d1f6e4
coquendi <corentin.pelhatre@laposte.net>
2019-10-09 23:24:45 +0200

    Creation du lecteur dans un dataframe + creation du corps du main

--- Src/Main/main.jl
+++ Src/Main/main.jl
@@ -1,1 +1,30 @@
 include("../Util/includes.jl")
+using Dates
+
+# Gestion de l'instance :
+instance = "A"
+reference = "022_3_4_EP_RAF_ENP"
+datas = lectureCSV(instance, reference)
+
+# Initialisation de la variable txt du fichier output :
+txt = string(
+    "===================================================\n",
+    "Etude de l'instance : ", instance, "\n",
+    "Reference du dossier : ", reference, "\n",
+    "A la date du : ", Dates.now(), "\n",
+    "===================================================\n\n"
+)
+
+# Initialisation des données :
+vehicles = datas[1]
+oo = datas[2] #optimization_objectives
+pbl = datas[3] #paint_batch_limi
+ratio = datas[4]
+
+
+
+
+
+
+# Ecriture dans le fichier output
+ecriture(txt, string("../../Output/", Dates.now(), ".txt"))
--- Src/Util/lecture.jl
+++ Src/Util/lecture.jl
@@ -1,60 +1,16 @@
+using CSV
+using DataFrames
+using Query
+
 # Fonction qui permet de lire un fichier csv
 # @param path l'adresse du fichier à etudier
-# @return la matrice associer au fichier
-function lectureCSV(path::String)
-    # Ouverture du flux
-    f=open(path)
-
-    # recuperation de la premiere ligne pour connaitre la taille de la matrice
-    ligneCourante = split(readline(f) , ";")
-
-    # taille de la matrice carré
-    taille = length(ligneCourante)
-
-    # Creation de la matrice de string from le csv
-    mat = zero(rand(taille,taille))
-
-    # Ajout de la premiere ligne :
-    for i = 1:taille
-        # ajout de la valeur courante
-        tmpLigne = ligneCourante[i]
-        tmp = tryparse(Float64, tmpLigne)
-        if typeof(tmp) == Nothing
-            mat[1,i] = valeurForCaractere(String(tmpLigne), mat, 1, i)
-        else
-            mat[1,i] = tmp
-        end
-
-    end
-
-    # Boucle sur toutes les lignes du fichier
-    for j = 2:taille
-        ligneCourante = split(readline(f) , ";")
-        for i = 1:taille
-            tmpLigne = ligneCourante[i]
-            tmp = tryparse(Float64, tmpLigne)
-            if typeof(tmp) == Nothing
-            # ajout de la valeur courante
-                mat[j,i] = valeurForCaractere(String(tmpLigne), mat, j, i)
-            else
-                mat[j,i] = tmp
-            end
-        end
-    end
-
-    # renvois de la matrice trouvée :
-    return mat
-end
-
-
-
-# Fonction qui permet de passé d'un caractère à sa valeur associé dans la matrice
-#
-# @param le caractère à étudier
-# @return la valeur associé
-function valeurForCaractere(c::String, mat::Array{Float64,2}, j::Int64, i::Int64)
-    if c == "-"
-        return 0
-    end
-    return mat[i,j]
+# @return (
+#           vehicles :: DataFrames.DataFrame
+#           optimization_objectives :: DataFrames.DataFrame
+#           paint_batch_limit :: DataFrames.DataFrame
+#           ratio :: DataFrames.DataFrame
+#           )
+function lectureCSV(instance::String, ref::String)
+    path = string("../../Input/Instances_set_", instance, "/", ref, "/")
+    return (CSV.read(string(path, "vehicles.txt")), CSV.read(string(path, "optimization_objectives.txt")), CSV.read(string(path, "paint_batch_limit.txt")), CSV.read(string(path, "ratios.txt")))
 end
