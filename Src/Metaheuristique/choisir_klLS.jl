4d787cad5f074293cef902ebd300f5e34ac235d6
Yekivan <50906077+Yekivan@users.noreply.github.com>
2019-10-27 23:04:42 +0100

    Update choisir_klLS.jl

--- Src/Metaheuristique/choisir_klLS.jl
+++ Src/Metaheuristique/choisir_klLS.jl
@@ -3,7 +3,7 @@
 # Cette fonction determine les entiers k et l d'ou sont applique la recherche locale, ainsi que l'algo de recherche locale a utiliser.
 # k et l dependant de si on utilise OptA, OptB ou OptC et aussi de la sequence_courante (pour savoir ou sont les blocks de couleur).
 # @param opt::Symbol : doit appartenir :OptA, :OptB, :OptC.
-# @return k::UInt32, l::UInt32 , idLS::Symbol : idLS doit appartenir a global ID_LS. Retourne les bonnes valeurs de k et de l en fonction de si on applique OptA, OptB ou OptC.
+# @return k::UInt32, l::UInt32 , idLS::Symbol : idLS doit appartenir a VFLS.ID_LS. Retourne les bonnes valeurs de k et de l en fonction de si on applique OptA, OptB ou OptC.
 function choisir_klLS(sequence_courrante::Array{Array{Int32,1},1}, opt::Symbol)
     #TODO : determiner les k et l en fonction de OptA, OptB et OptC et de la sequence courrante.
     return k, l, idLS
