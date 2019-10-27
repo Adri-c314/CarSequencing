0ab7620858c43e32c3a9fedd57f58072db6a8833
Yekivan <50906077+Yekivan@users.noreply.github.com>
2019-10-27 22:54:06 +0100

    Create reflection.jl

---
+++ Src/Metaheuristique/reflection.jl
@@ -0,0 +1,7 @@
+# Squelette de la fonction de recherche locale reflection, par Xavier
+
+function reflection!(sequence_courrante::Array{Array{Int32,1},1}, score_courrant::Array{Int32,1}, k::UInt32, l::UInt32)
+    #TODO : Effectuer la recherche locale
+    #TODO : L'evaluer et si elle est meilleure (a 100*(R-1) % pres), garder les modifs, sinon les retirer de sequence_courrante.
+    nothing # Pas de return pour eviter les copies de memoire.
+end
