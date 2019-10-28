2bee701dc8ffb8ea793dc9f3af53fdfff0b19e22
Yekivan <50906077+Yekivan@users.noreply.github.com>
2019-10-28 19:19:30 +0100

    Update VFLS.jl

--- Src/Metaheuristique/VFLS.jl
+++ Src/Metaheuristique/VFLS.jl
@@ -1,4 +1,4 @@
-# Fonction principale de l'heuristique, par Xavier
+# Fonction principale de l'heuristique, par Xavier.
 
 include("choisir_klLS.jl")
 
@@ -7,7 +7,7 @@ const OPT = (:OptA, :OptB, :OptC) #Macro pour identifier les algos OptA, OptB et
 const ID_LS = (:swap!, :fw_insertion!, :bw_insertion!, :reflection!, :permutation!) #Macro pour identifier les fonctions de LS
 
 function VFLS(data, temps_max::Float32 = 10.0)
-    sequence_courrante::Array{Array{Int32,1},1} = init_sequence(data) #Le glouton
+    sequence_courrante::Array{Array{Int32,1},1} = init_sequence(data) #Le glouton.
     score_courrant::Array{Int32,1} = evaluation_init(sequence_courrante) #Score = tableaux des scores des 3 objectifs respectifs.
     sequence_meilleure = deepcopy(sequence_courrante)
     score_meilleur = deepcopy(score_courrant)
@@ -15,8 +15,8 @@ function VFLS(data, temps_max::Float32 = 10.0)
     global OPT
     while temps_max > time() - debut
         for opt in OPT
-            local k::UInt32, l::UInt32, LSfoo::Function = choisir_klLS(sequence_courrante, opt) #Choix des param de la LS
-            LSfoo(sequence_courrante, k, l, score_courrant) #Appel de la fonction de nom (Symbol) LS
+            local k::UInt32, l::UInt32, LSfoo!::Function = choisir_klLS(sequence_courrante, opt) #Choix des param de la LS.
+            LSfoo!(sequence_courrante, k, l, score_courrant) #Appel de la fonction de recherche locale.
             if estMieux(score_courrant, score_meilleur)
                 sequence_meilleure = deepcopy(sequence_courrante)
                 score_meilleur = deepcopy(sequence_courrante)
